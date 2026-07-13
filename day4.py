8f435f4b686ff15bf1176f23c27bc711c2db9579
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-07-13 11:00:31 +0530

    string slice

--- day4.py
+++ day4.py
@@ -10,6 +10,9 @@
 # [ Microprocessor (CPU) ] ──► Executes the program
 
 # variable is a name given to the memory location in order to store data in a RAM (Random Access Memory) of a computer. It is a symbolic name that represents a memory location where data can be stored and retrieved during program execution.
+from h11 import Data
+
+
 a =10
 print(a)
 b="sachi"
@@ -23,3 +26,16 @@ print(x, y, z,type(x), type(y), type(z))
 
 p,q,r = 10, 20, 30
 print(p,q,r)
+
+# single line comment
+
+""" this is a multi line or block comment """
+
+# data types in python are:
+# 1. Numeric Data Types: int, float, complex
+# 2. Text Data Type: str
+# 3. Boolean Data Type: bool
+# 4. Sequence Data Types: list, tuple, range
+# 5. Mapping Data Type: dict
+# 6. Set Data Types: set, frozenset
+
